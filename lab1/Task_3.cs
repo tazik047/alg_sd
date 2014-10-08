33e9bf88ed580887e111f655f1aca6c56d16ff97
annhv <annhv@yandex.ru>
2014-10-09 00:07:33 +0300

    Task 3

--- lab1/Task_3.cs
+++ lab1/Task_3.cs
@@ -8,6 +8,14 @@ namespace lab1
 {
     static class Task_3
     {
-        //типа код
+        public static List<int> Sort(List<int> list)
+        {
+            if (list.Count <= 1) return list;
+
+            List<int> left = list.GetRange(0, list.Count / 2);
+            List<int> right = list.GetRange(left.Count, list.Count - left.Count);
+            return Task_2.Merge(Sort(left), Sort(right));
+        }
+
     }
 }
