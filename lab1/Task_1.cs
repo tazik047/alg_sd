dc45aec84ad6ae4b2f3bf3de931fda275cd96a2d
Stas <stas249501@gmail.com>
2014-10-08 22:00:12 +0300

    Первое задание вроде сделал. Рекурсия немного напрягает. Лучшего не придумал

--- lab1/Task_1.cs
+++ lab1/Task_1.cs
@@ -13,6 +13,9 @@ namespace lab1
         /// и вставляем его на нужную позицию в уже отсортированном списке, до тех пор,
         /// пока набор входных данных не будет исчерпан.
         /// 
+        /// O(n^2)
+        /// 
+        /// 
         /// 1) 5 6 3 1 8 7 2 4 выбранный элемент 5.
         /// 2) 5 6 3 1 8 7 2 4 выбранный элемент 6.
         /// 3) 5 6 3 1 8 7 2 4 выбранный элемент 3. Нужно менять ->
@@ -25,12 +28,78 @@ namespace lab1
         /// 
         /// </summary>
         /// <param name="mas">Входной массив</param>
-        public void sortSimple(List<int> mas)
+        public static void sortSimple(List<int> mas)
         {
-            for (int i = 0; i < mas.Count - 1; i++)
+            for (int i = 1; i < mas.Count; i++)
             {
-
+                if (mas[i - 1] > mas[i])
+                {
+                    int j = i - 1;
+                    int temp = mas[i];
+                    while (j >= 0 && mas[j] > temp)
+                        mas[j + 1] = mas[j--];
+                    mas[++j] = temp;
+                }
             }
+        }
+
+        /// <summary>
+        /// Обычный бинарный поиск.
+        /// </summary>
+        /// <param name="mas">Сам массив</param>
+        /// <param name="el">элемент для поиска</param>
+        /// <param name="r">правая граница</param>
+        /// <param name="l">левая граница</param>
+        /// <returns></returns>
+        private static int binSearch(List<int> mas, int el, int r, int l = 0)
+        {
+            int index = (l + r) / 2;
+            if (l > r)
+                return l;
+            else if (el == mas[index])
+                return index;
+            else if (el > mas[index])
+                return binSearch(mas, el, r, index + 1);
+            else
+                return binSearch(mas, el, index - 1, l);
+        }
+
+        /// <summary>
+        /// Вставками, использую бинарную сортировку.
+        /// по идеи O(n*log n) 
+        /// </summary>
+        /// <param name="mas">Сам массив</param>
+        public static void sortWithBinSearch(List<int> mas)
+        {
+            for (int i = 1; i < mas.Count; i++)
+            {
+                if (mas[i - 1] > mas[i])
+                {
+                    int temp = mas[i];
+                    int ind = binSearch(mas, temp, i - 1);
+                    mas.RemoveAt(i);
+                    mas.Insert(ind, temp);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Все тоже, но теперь с рекурсеей.
+        /// </summary>
+        /// <param name="mas">Сам массив</param>
+        /// <param name="i">Начало неотсортированной части</param>
+        public static void sortWithBinAndReqursion(List<int> mas, int i = 1)
+        {
+            if (i >= mas.Count)
+                return;
+            if (mas[i - 1] > mas[i])
+            {
+                int temp = mas[i];
+                int ind = binSearch(mas, temp, i - 1);
+                mas.RemoveAt(i);
+                mas.Insert(ind, temp);
+            }
+            sortWithBinAndReqursion(mas, ++i);
         }
     }
 }
