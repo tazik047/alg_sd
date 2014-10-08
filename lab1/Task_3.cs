ea023cc79265063798c350121001b9ac550239f6
annhv <annhv@yandex.ru>
2014-10-09 00:09:51 +0300

    переименование метода в таске 3

--- lab1/Task_3.cs
+++ lab1/Task_3.cs
@@ -8,13 +8,13 @@ namespace lab1
 {
     static class Task_3
     {
-        public static List<int> Sort(List<int> list)
+        public static List<int> RecMergeSort(List<int> list)
         {
             if (list.Count <= 1) return list;
 
             List<int> left = list.GetRange(0, list.Count / 2);
             List<int> right = list.GetRange(left.Count, list.Count - left.Count);
-            return Task_2.Merge(Sort(left), Sort(right));
+            return Task_2.Merge(RecMergeSort(left), RecMergeSort(right));
         }
 
     }
