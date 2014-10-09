baa07fc006bbb245cfaf60382b498ad09354ac47
Stas <stas249501@gmail.com>
2014-10-09 11:01:01 +0300

    Добавил класс для расчета времени работы

---
+++ lab1/CountTime.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountTimeAndExport2EXCEL
+{
+    class CountTime
+    {
+        Action<List<int>> function;
+        Stopwatch counter;
+
+        public CountTime(Action<List<int>> func)
+        {
+            function = func;
+            counter = new Stopwatch();
+        }
+
+        public IEnumerable<Element> StartCount()
+        {
+            var mas = new List<Element>();
+            long countELem = 1;
+            for (int i = 0; i < 25; i++)
+            {
+                List<int> m = new List<int>();
+                Random rnd = new Random();
+                for (int j = 0; j < countELem; j++)
+                {
+                    m.Add(rnd.Next());
+                }
+                counter.Reset();
+                counter.Start();
+                //function(m);
+                function.Invoke(m);
+                counter.Stop();
+                mas.Add(new Element(countELem, counter.ElapsedMilliseconds));
+                countELem *= 2;
+            }
+            return mas;
+        }
+    }
+}
---
+++ lab1/Element.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountTimeAndExport2EXCEL
+{
+    struct Element
+    {
+        public long Count;
+        public long Time;
+
+        public Element(long c, long t)
+        {
+            Count = c;
+            Time = t;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Количество:{0} Время:{1}", Count, Time);
+        }
+    }
+}
