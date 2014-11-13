f69842564231e749826690a72f2dc749ba991c10
Stas <stas249501@gmail.com>
2014-11-13 20:16:23 +0200

    начало 3 лабы

---
+++ lab3/lab3/Tree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Tree
+    {
+        public Tree Left { get; set; }
+        public Tree Right { get; set; }
+
+        public int Value { get; set; }
+
+        public Tree(int v)
+        {
+            Value = v;
+        }
+    }
+}
